adabdcb3f324d7e55e05ccf49cff2b67e5083d55
yaelshtaiman <yaelshtaiman@gmail.com>
2024-11-27 15:02:36 +0200

    email section and footer, not done

---
+++ app/components/EmailSection.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import GithubIcon from "../../public/github-icon.svg";
+import LinkedinIcon from "../../public/linkedin-icon.svg";
+import Link from "next/link"
+import Image from "next/image"
+
+
+const EmailSection = () => {
+    return <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+        <div
+            className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"
+        >
+
+        </div>
+        <div>
+            <h5 className="text-xl font-bold text-white my-2">Keep in touch.</h5>
+            <p className="text-[#ADB7BE] mb-4 max-w-md">
+                {" "}
+                Feel free to reach out to me with your questions, ideas, or just to say hello
+            </p>
+            <div className="socials flex flex-row gap-2">
+                <Link href="https://github.com/yaelshteiman/">
+                    <Image src={GithubIcon} alt="Github Icon" />
+                </Link>
+                <Link href="https://www.linkedin.com/in/yaelshteiman/">
+                    <Image src={LinkedinIcon} alt="Linkedin Icon" />
+                </Link>
+            </div>
+        </div>
+        <div>
+            <form className="flex flex-col">
+                <div className="mb-6">
+                    <label
+                        htmlFor="email"
+                        className="text-white block mb-2 text-sm font-medium"
+                    >
+                        Your Email
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        required
+                        className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                        placeholder="yourname@gmail.com"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label
+                        htmlFor="subjecr"
+                        className="text-white block text-sm mb-2 font-medium"
+                    >
+                        Subject
+                    </label>
+                    <input
+                        type="text"
+                        id="subject"
+                        required
+                        className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                        placeholder="Just saying hello"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label
+                        htmlFor="message"
+                        className="text-white block text-sm mb-2 font-medium"
+                    >
+                        Message
+                    </label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                        placeholder="Let's talk about..."
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+                >
+                    Send Message
+                </button>
+            </form>
+        </div>
+    </section>
+};
+
+export default EmailSection;
