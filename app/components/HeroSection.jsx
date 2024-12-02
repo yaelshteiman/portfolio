124d51052eb593a03d2a27d4e6b3e2abdaf60063
yaelshtaiman <yaelshtaiman@gmail.com>
2024-12-02 17:18:05 +0200

    resume instead of download resume

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -36,7 +36,7 @@ const HeroSection = () => {
                             className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-teal-500 via-cyan-600 to-sky-700 hover:bg-slate-200 text-white mt-3"
                         >
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                                Download Resume
+                                Resume
                             </span>
 
                         </Link>
