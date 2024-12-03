8fd8b5e602c51072c43e714457b2f1945a330717
yaelshtaiman <yaelshtaiman@gmail.com>
2024-12-03 14:47:31 +0200

    make the github link open in a new tab

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -13,6 +13,8 @@ const ProjectCard = ({imgUrl, title, description, gitUrl}) => {
                     <Link
                         href={gitUrl}
                         className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white"
+                        target="_blank"
+                        rel="noopener noreferrer"
                     >
                         <LinkIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
                     </Link>
