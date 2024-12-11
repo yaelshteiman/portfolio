c6270943b35efc729ea678921c99c80b387f1a6e
yaelshtaiman <yaelshtaiman@gmail.com>
2024-12-11 13:14:09 +0200

    changing hidden photos

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -78,7 +78,7 @@ const ProjectsSection = () => {
             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12 font-serif">
                 My Projects
             </h2>
-            <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+            <div className="text-white hidden sm:flex md:flex flex-row justify-center items-center gap-2 py-6">
                 <ProjectTag
                     onClick={handleTagChange}
                     name="All"
